dda2400305eec8ffc4c8718c0c84ad1347399a20
Fatima Azzahro <azzahro.fatima@gmail.com>
2022-12-13 12:51:44 +0700

    API Testing - Add test for POST a new user

---
+++ test.js
@@ -0,0 +1,13 @@
+const request = require("supertest")("https://reqres.in");
+const expect = require("chai").expect;
+
+describe("Create User Reqrest", function () {
+    it("Success Create User", async function () {
+        const response = await request
+            .post("/api/users")
+            .send({ name: "sanbercode", job: "QA bootcamp" });
+
+        expect(response.body.name).to.eql('sanbercode');
+        expect(response.body.job).to.eql('QA bootcamp');
+    })
+})
